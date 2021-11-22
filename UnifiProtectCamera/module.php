060bb91f5f30d713f2e7345bb48397b30010874e
Martin Kleinhans <kleinhans.martin@bcg.com>
2021-11-22 20:46:37 +0100

    Update

--- UnifiProtectCamera/module.php
+++ UnifiProtectCamera/module.php
@@ -17,7 +17,7 @@ class UnifiProtectCamera extends IPSModule
         $this->RegisterVariableBoolean("IsMotionDetected", "Is Motion Detected");
 
         $uuid = $this->ReadPropertyString('uuid');
-        $this->SetReceiveDataFilter('.*'.preg_quote('\"id\":\"'.($uuid ? $uuid : 'xxxxxxxx').'\"').'.*');
+     //   $this->SetReceiveDataFilter('.*'.preg_quote('\"id\":\"'.($uuid ? $uuid : 'xxxxxxxx').'\"').'.*');
     }
 
     /**
@@ -27,7 +27,7 @@ class UnifiProtectCamera extends IPSModule
     {
         parent::ApplyChanges();
         $uuid = $this->ReadPropertyString('uuid');
-        $this->SetReceiveDataFilter('.*'.preg_quote('\"id\":\"'.($uuid ? $uuid : 'xxxxxxxx').'\"').'.*');
+       // $this->SetReceiveDataFilter('.*'.preg_quote('\"id\":\"'.($uuid ? $uuid : 'xxxxxxxx').'\"').'.*');
         $this->SendDebug('filter', '.*'.preg_quote('\"id\":\"'.($uuid ? $uuid : 'xxxxxxxx').'\"').'.*', 0);
     }
 
